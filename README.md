e7b21ed086afdb0e0be1bb8586e23ee65e27f9be
nwpcgn <git@stackblitz.com>
2025-08-09 13:24:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# roguegame-1-design
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/nwpcgn/roguegame-1-design)
---
+++ src/daten.js
@@ -0,0 +1,82 @@
+export const lorem = [
+	{
+		heading: 'American Marigold',
+		sub: 'Tagetes',
+		bame: 'Asteraceae',
+		info: 'Tagetes is a genus of annual or perennial, mostly herbaceous plants in the sunflower family Asteraceae.',
+		img: 'https://i.imgur.com/mh1zGfs.jpeg',
+		url: 'https://google.com/search?q=Tagetes'
+	},
+	{
+		heading: 'Blue Sage',
+		sub: 'Salvia',
+		bame: 'Lamiaceae',
+		info: 'Popular spring and summer bedding plant, hardy in sunny areas, drought tolerant.',
+		img: 'https://i.imgur.com/eJSYh97.jpeg',
+		url: 'https://google.com/search?q=Salvia'
+	},
+	{
+		heading: 'Petunia',
+		sub: 'Petunia',
+		bame: 'Solanaceae',
+		info: 'Petunias can be found in every color of the rainbow in solids, contrasting veins or edges, and star patterns.',
+		img: 'https://i.imgur.com/8me0gFu.jpeg',
+		url: 'https://google.com/search?q=Petunia'
+	},
+	{
+		heading: 'Eastern Redbud',
+		sub: 'Cercis',
+		bame: 'Fabaceae',
+		info: 'Best grown in a well-drained soil in full sun to part shade',
+		img: 'https://i.imgur.com/GrBguUj.jpeg',
+		url: 'https://google.com/search?q=Cercis'
+	},
+	{
+		heading: 'China Pink',
+		sub: 'Dianthus',
+		bame: 'Caryophyllaceae',
+		info: 'Dianthus has found a spot in cottage gardens for generations.',
+		img: 'https://i.imgur.com/TpT76rx.jpeg',
+		url: 'https://google.com/search?q=Dianthus'
+	},
+	{
+		heading: 'Common Zinnia',
+		sub: 'Zinnia',
+		bame: 'Asteraceae',
+		info: 'Zinnias are a fantastic garden flower that have been around for generations.',
+		img: 'https://i.imgur.com/eGvr9mO.jpeg',
+		url: 'https://google.com/search?q=Zinnia'
+	},
+	{
+		heading: 'Pink Cosmos',
+		sub: 'Cosmos',
+		bame: 'Asteraceae',
+		info: 'Spanish priests in Mexico named these cheery little blooms because of their evenly placed, orderly petals.',
+		img: 'https://i.imgur.com/cX2a39p.jpeg',
+		url: 'https://google.com/search?q=Cosmos'
+	},
+	{
+		heading: 'Lavender',
+		sub: 'Lavendula',
+		bame: 'Lamiaceae',
+		info: 'Lavender is a bushy, strong-scented perennial plant from the Mediterranean.',
+		img: 'https://i.imgur.com/XtgHewF.jpeg',
+		url: 'https://google.com/search?q=Lavendula'
+	},
+	{
+		heading: 'Garden Lily',
+		sub: 'Lilium',
+		bame: 'Liliaceae',
+		info: 'Lilies prefer a position that gets some sun, but will grow well in sites that are in light shade for just part of the day.',
+		img: 'https://i.imgur.com/3UNmGXG.jpeg',
+		url: 'https://google.com/search?q=Lilium'
+	},
+	{
+		heading: 'Field Pansy',
+		sub: 'Viola',
+		bame: 'Violaceae',
+		info: 'Violas have been a gardener favorite for centuries.',
+		img: 'https://i.imgur.com/nkuRuaq.jpeg',
+		url: 'https://google.com/search?q=Viola'
+	}
+]
